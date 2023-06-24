fcb4deaaf83719bae90d27b3c925f87f5089ff27
Juliana Santos <70862676+Julianavdsantos@users.noreply.github.com>
2023-06-24 17:33:20 -0300

    Update and rename starWars.js to apiService.js
    Melhoria: Criação de microserviçoes

---
+++ apiService.js
@@ -0,0 +1,68 @@
+ 
+
+import tabelaService from './tabelaService.js';
+
+
+const apiService = {
+     buscarNaApi: function(url, headers) {
+        fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            tabelaService.atualizarTable(data.results, headers);
+            
+          })
+          .catch(error => {
+            console.log('Erro ao obter os dados da API:', error);
+          });
+      }
+
+};
+export default apiService;
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('films-link').addEventListener('click', function() {
+      const url = 'https://swapi.dev/api/films/';
+      const headers = ['Title', 'Director', 'Producer'];
+      apiService.buscarNaApi(url, headers);
+
+     
+    });
+  
+    document.getElementById('people-link').addEventListener('click', function() {
+        debugger
+      const url = 'https://swapi.dev/api/people/';
+      const headers = ['Name', 'Height', 'Mass'];
+      apiService.buscarNaApi(url, headers);
+
+    });
+  
+    document.getElementById('planets-link').addEventListener('click', function() {
+      const url = 'https://swapi.dev/api/planets/';
+      const headers = ['Name', 'Population', 'Gravity'];
+      apiService.buscarNaApi(url, headers);
+
+    });
+  
+    document.getElementById('species-link').addEventListener('click', function() {
+      const url = 'https://swapi.dev/api/species/';
+      const headers = ['Name', 'Classification', 'Designation'];
+      apiService.buscarNaApi(url, headers);
+
+    });
+  
+    document.getElementById('starships-link').addEventListener('click', function() {
+      const url = 'https://swapi.dev/api/starships/';
+      const headers = ['Name', 'Model', 'Passengers'];
+      apiService.buscarNaApi(url, headers);
+
+    });
+  
+    document.getElementById('vehicles-link').addEventListener('click', function() {
+      const url = 'https://swapi.dev/api/vehicles/';
+      const headers = ['Name', 'Model', 'Passengers'];
+      apiService.buscarNaApi(url, headers);
+
+    });
+  });
+  
